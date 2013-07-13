9d079a7685b087dd7a3ebac7c2dbb60117e6601d
lukerich <r.l.luke@fullsail.edu>
2013-07-13 10:46:34 -0400

    updates

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -3,4 +3,14 @@
  * Date: 7/13/13
  * Goal1: Assignment: The Duel - Part I
  */
+                  // assignment of initial variables
+    var plyOneNam = "Spiderman";
+    var plyTwoNam = "Batman";
+
+    var plyOneHlt = 100;
+    var plyTwoHlt = 100;
+
+    var plyOneDam = 50;
+    var plyTwoDam = 50;
+
 
