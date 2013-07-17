ae426ddf9d21d33be1f770c484cf808268f5441e
lukerich <r.l.luke@fullsail.edu>
2013-07-16 22:13:51 -0400

    updates

--- Wk1/goal2_practice/js/main.js
+++ Wk1/goal2_practice/js/main.js
@@ -175,7 +175,7 @@ var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 //STUDENT ACTIVITY 1:
 // Use substr() to console.log 'JavaScript!' from str1.
-
+        console.log(str1.substr(7,11));
 
 // Use concat() to console.log 'I love JavaScript ! JavaScript loves me!' with str1 and str2.
 
