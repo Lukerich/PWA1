db26b36d3fd3937d9b5667227c84f39ec3914c02
lukerich <r.l.luke@fullsail.edu>
2013-07-19 17:58:45 -0400

    updates

--- Wk1/goal2_assign_JSpractice/js/main.js
+++ Wk1/goal2_assign_JSpractice/js/main.js
@@ -53,15 +53,15 @@
     var avgNumbers = function(arr){
             console.log(arr);         // I get the list out of the array
 
-            var couNter = 0;
-            for (var i=0, j=arr.length; i<=j; i++){
-                console.log(couNter);  // starts out as 0  then increments 1 to 15
+            var counter = 0;
+            for (var i=0, j=arr.length; i<j; i++){
+                console.log(counter);  // starts out as 0  then increments 1 to 15
                 console.log(j);        // I get a 5
                 console.log(i);        // starts at 0 and increments to 5
-                couNter +=arr[i];
+                counter +=arr[i];
                 };
-            console.log("conuter is s" + couNter);        // outputs NaN code
-            var numAvg = (couNter/arr.length);
+            console.log("conuter is " + counter);        // outputs NaN code
+            var numAvg = (counter/arr.length);
 
             console.log(arr.length);   // I get the number of elements in the array (5)
             console.log("Avg is " + numAvg);        //I get NaN
@@ -73,12 +73,9 @@
     //--------------------------------------------------------
     console.log("2. concat first and last name");
 
-        var fullName = function(){
-         fullName.join(" ");   //PUT FUNCTION HERE
-         return namFull;
-        };
 
-    console.log(fullName('James', 'Bond'));
+
+    console.log(fullName(['James', 'Bond']));
 
     //--------------------------------------------------------
     console.log("3. word count");
