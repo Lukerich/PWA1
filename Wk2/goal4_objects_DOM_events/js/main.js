978e9ff606d44b37f7445d98aba3589dbfcca742
lukerich <r.l.luke@fullsail.edu>
2013-07-22 20:57:14 -0400

    updates

--- Wk2/goal4_objects_DOM_events/js/main.js
+++ Wk2/goal4_objects_DOM_events/js/main.js
@@ -355,9 +355,9 @@ console.log('------------ DOCUMENT OBJECT MODEL -------------------');
 
 console.log('------------ getElementById -------------------');
 
-
-
-
+        var playBox = document.getElementById('playbox');
+        console.log(playBox);
+        playBox.style.backgroundColor ='Red';
 /*
 	==================================================================
 	document.getElementsByTagName(string);
@@ -368,9 +368,14 @@ console.log('------------ getElementById -------------------');
 
 console.log('------------ getElementsByTagName -------------------');
 
+        var anchors = document.getElementsByTagName('a');
+        console.log(anchors);
 
+        console.log(anchors[1]);
 
-
+        for (i=0, max=anchors.length; i<max; i++){
+            console.log(anchors[i]);
+        }
 /*
 	==================================================================
 	document.querySelectorAll(string);
@@ -384,7 +389,11 @@ console.log('------------ getElementsByTagName -------------------');
 
 console.log('------------ querySelectorAll -------------------');
 
+        var nav = document.querySelectorAll('#nav li:last-child');
+        console.log(nav);
 
+        var cf = document.querySelectorAll('.clearfix');
+        console.log(cf);
 
 
 /*
@@ -398,8 +407,8 @@ console.log('------------ querySelectorAll -------------------');
 */
     console.log('------------ querySelector -------------------');
 
-
-
+        var nav = document.querySelector('#nav');
+        console.log(nav);
 
 
     /*
