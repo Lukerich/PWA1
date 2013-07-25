ac37b7240dd6acd63a07b5e53e7826819a5a8217
lukerich <r.l.luke@fullsail.edu>
2013-07-24 23:48:18 -0400

    updates

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -9,10 +9,6 @@
 (function(){
     console.log("FIGHT!!!");
 
-    //player arrays
-//    var playerOne = ["Spiderman", 20, 100];
-//    var playerTwo = ["Batman", 20, 100];
-    //player objects
 
     var plyLst = {players:[
                     {name:'Spiderman', damage:20, health:100},
@@ -20,51 +16,60 @@
     ]
     };
 
+        var onClk = document.body.id.['fight_btn'].onclick = function(e){
+            fight()(e);
+
+            e.preventDefault();
+            return false;
+        };
+
+
     //initiate round
     var round=1;
 
     function fight(){
-        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]);
-        for (var i = 0; i < 10; i++)
-        {
-            var f1 = Math.floor((Math.random()*playerOne[1])+playerOne[1] *.5);
-            var f2 = Math.floor((Math.random()*playerTwo[1])+playerTwo[1] *.5);
 
-            //inflict damage
-            playerOne[2]-=f1;
-            playerTwo[2]-=f2;
+        alert(plyLst.players[0].name+":"+plyLst.players[0].health+plyLst.players[1].name+":"+plyLst.players[1].health);
 
-            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);
+        for (var i = 0; i < 10; i++){
+                var f1 = Math.floor((Math.random()*plyLst.players[0].damage)+plyLst.players[0].damage *.5);
+                var f2 = Math.floor((Math.random()*plyLst.players[1].damage)+plyLst.players[1].damage *.5);
 
-            //check for victor
-            var result = winnerCheck();
-            console.log(result);
-            if (result==="no winner")
-            {
-                round++;
-                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]);
+                //inflict damage
+                plyLst.players[0].health-=f1;
+                plyLst.players[1].health-=f2;
 
-            } else{
-                alert(result);
-                break;
-            };
+                console.log(plyLst.players[0].name+": "+plyLst.players[0].health + " " + plyLst.players[1].name+":"+plyLst.players[1].health);
 
-        };
-    };
+                //check for victor
+                var result = winnerCheck();
+                console.log(result);
+                if (result==="no winner")
+                {
+                            round++;
+                            alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]);
 
-    function winnerCheck(){
-        var result="no winner";
-        if (playerOne[2] < 1 && playerTwo[2] < 1)
-        {
-            result = "You Both Die";
-        } else if(playerOne[2] < 1){
-            result =playerTwo[0]+" WINS!!!"
-        } else if (playerTwo[2] < 1)
-        {
-            result = playerOne[0]+" WINS!!!"
-        };
-       return result;
-    };
+                        } else{
+                            alert(result);
+                            break;
+                        };
+
+                    };
+                };
+
+                function winnerCheck(){
+                    var result="no winner";
+                    if (playerOne[2] < 1 && playerTwo[2] < 1)
+                    {
+                        result = "You Both Die";
+                    } else if(playerOne[2] < 1){
+                        result =playerTwo[0]+" WINS!!!"
+                    } else if (playerTwo[2] < 1)
+                    {
+                        result = playerOne[0]+" WINS!!!"
+                    };
+                    return result;
+                };
 
     fight();
 
