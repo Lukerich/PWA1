a8649a98cb4ab09da6cd092be0bde8fddb14eeb0
lukerich <r.l.luke@fullsail.edu>
2013-07-25 17:30:07 -0400

    updates

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -17,38 +17,34 @@
 
 
     //initiate round
+    var round = 1;
     var fhtBtn = document.querySelector("#fight_btn");
-    var round = document.querySelector("#round_number input");
-
-    console.log(round);
     fhtBtn.onclick = fight;
     function fight(){
                     console.log("in fight");
                     var scrLks = document.querySelectorAll('#scores p');
                         scrLks[0].innerHTML = ('id', players[0].name+':'+players[0].health);
                         scrLks[1].innerHTML = ('id', players[1].name+':'+players[1].health);
-                        console.log(scrLks[0].innerHTML + scrLks[1].innerHTML);
+
             for (var i = 0; i < 10; i++){
                     var f1 = Math.floor((Math.random()*players[0].damage)+players[0].damage *.5);
                     var f2 = Math.floor((Math.random()*players[1].damage)+players[1].damage *.5);
-                     console.log(f1 + f2);
+
                     //inflict damage
                     players[0].health-=f1;
                     players[1].health-=f2;
-
-                    console.log(players[0].name+": "+players[0].health + " " + players[1].name+":"+players[1].health);
 
                     //check for victor
                     var result = winnerCheck();
                     console.log(result);
                     if (result==="no winner")
                     {
-                    round++;
+                        round++;
+                        console.log(round);
+                        var round = document.querySelectorAll('#round_number input');
                         var scrLks = document.querySelectorAll('#scores p');
-                        scrLks[0].innerHTML = ('id',f1);
-                        scrLks[1].innerHTML = ('id',f2);
-
-                        console.log(scrLks[0].innerHTML + scrLks[1].innerHTML);
+                        scrLks[0].innerHTML = ('id', players[0].name+':'+players[0].health);
+                        scrLks[1].innerHTML = ('id', players[1].name+':'+players[1].health);
                 }else{
                     scrRsl = document.querySelector('scores');
                     scrRsl= result;
