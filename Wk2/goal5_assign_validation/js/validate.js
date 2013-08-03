114c082bff5a487d97c2f380531ad835d5d07b0e
lukerich <r.l.luke@fullsail.edu>
2013-08-03 12:39:54 -0400

    updates

--- Wk2/goal5_assign_validation/js/validate.js
+++ Wk2/goal5_assign_validation/js/validate.js
@@ -6,18 +6,23 @@
 (function(){
 
 
-    myform.onsubmit = function(e){
+        var button = document.getElementById('f_submit');
 
-        var inputName = document.querySelector("#f_email");
-        console.log(inputName.name);
-        console.log(inputName.value);
-        console.log(inputName.nextSibling);
-        console.log(inputName.nextSibling.nextSibling);
-        console.log(inputName.nextSibling.nextSibling.nextSibling);
-        console.log(inputName.nextSibling.nextSibling.nextSibling.nextSibling);
-        //console.log(document.getElementById('f_username').value);
-        //console.log(document.getElementById('f_username'));
-        validateField(inputName.name);
+        var myForm = function(e){
+
+
+            var f_username = document.getElementById('f_username')
+            var f_email = document.getElementById('f_email')
+            var f_phone = document.getElementById('f_phone')
+            var f_password = document.getElementById('f_password')
+            var f_ssn = document.getElementById('f_ssn')
+
+            console.log(f_username.value);
+            console.log(f_email.value);
+            console.log(f_phone.value);
+            console.log(f_password.value);
+            console.log(f_ssn.value);
+
 //        validateField(id);
 //        validateField(id);
 //        validateField(id);
@@ -67,45 +72,10 @@
             inputName.style.backgroundColor = 'white';
         }
 
-//    myform.onsubmit = function(e){
-//
-//        //Below is one example of the validateField call with an argument.
-//        //You must dynamically retrieve the ID name from the DOM/HTML.
-//
-//        validateField(id);  //id = is the form input field ID
-//
-//
-//        e.preventDefault();
-//        return false;
-//    };
-//
-//
-//    var validateField = function(inputName){
-//
-//        if (inputName.name === 'id name goes here'){
-//            var pattern = 'a RegEx pattern goes here'
-//
-//            //You will need to create an else-if statement for each input field id.  The
-//            //      format will be similar to the above IF statement.
-//
-//
-//        };
-//
-//        var pass = 'the RegEx .test statement is needed here';
-//        var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
-//
-//        if (!pass || inputName.value.length < 2){
-//            errorMsg.style.display='block';
-//            inputName.style.backgroundColor = 'red';
-//        } else if (pass && inputName.value.length > 5){
-//            errorMsg.style.display='none';
-//            inputName.style.backgroundColor = 'green';
-//        } else {
-//            errorMsg.style.display='none';
-//            inputName.style.backgroundColor = 'white';
-//        };
-//    };
     };
+
+    button.onclick = myForm;
+
 })();  // end wrapper
 
 
