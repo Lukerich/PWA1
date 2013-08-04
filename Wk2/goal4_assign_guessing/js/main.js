73b1c3923a6b830b8678c2e5f2c9e1f6509f3b2b
lukerich <r.l.luke@fullsail.edu>
2013-08-04 04:07:10 -0400

    updates

--- Wk2/goal4_assign_guessing/js/main.js
+++ Wk2/goal4_assign_guessing/js/main.js
@@ -9,6 +9,46 @@
 
 //Game variables
 (function (){
+         console.log(document.getElementsByTagName("button"));
+    var buTn = document.getElementsByTagName('button');
 
+    var cliKs = 0;
+
+    buTn.onclick = function(){
+
+         console.log("in function");
+
+        var inPut = document.getElementById('input');
+            console.log(inPut);
+        var magNum = Math.floor(Math.random() * 10);
+            console.log(magNum);
+
+        if (cliKs < 2){
+            if (inPut < magNum){
+                console.log("To Low");
+            } else if (inPut > magNum){
+                console.log("To High");
+            }else if (inPut === magNum){
+                console.log("You Win!!");
+                gmOv();
+            }                                            // If statement for clicks
+        clKs();                                            //  calling clKs function
+        }else{                                                     //else to end display and disable button
+         gmOv();
+        }
+
+    e.preventDefault();                                 // prevent to stop function
+    return false;
+    };
+
+    function gmOv(){
+        var inBt = document.getElementsByTagName();       // variable to assign button
+        inBt.innerHTML = ("DONE!!!");                            // assign DONE!!!
+        document.onclick = function() {return false;};           //DOM to disable button
+    }
+
+    function clKs(){                                             // click function
+        document.getElementById('guess').value = ++cliKs;     // getElementById to add up clicks
+    }
 
 })();
