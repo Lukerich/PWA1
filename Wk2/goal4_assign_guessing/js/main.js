4c405ea31a6c463ef4624aa3958e853e84a656c8
lukerich <r.l.luke@fullsail.edu>
2013-08-04 21:10:40 -0400

    updates

--- Wk2/goal4_assign_guessing/js/main.js
+++ Wk2/goal4_assign_guessing/js/main.js
@@ -10,29 +10,26 @@
 //Game variables
 (function (){
 
-    console.log('Start function');
     var buT = document.querySelector("button");
-    console.log(buT);
+
     var cliKs = 0;
     console.log(cliKs);
+
     var magNum = Math.floor(Math.random() * 10 + 1);
     console.log(magNum);
 
     buT.onclick = function(e){
 
-        console.log("in function");
+        var inPut = document.getElementById('input').value;    // variable assigned to user input
 
-        var inPut = document.getElementById('input').value;
-            console.log(inPut);
-
-        if (cliKs < 3){
+        if (cliKs < 3){                                 // if else logic to evaluate input against Magic Number
             if (inPut < magNum){
                 document.getElementById("output").innerHTML = ("To Low");
             } else if (inPut > magNum){
                 document.getElementById("output").innerHTML = ("To High");
             }else if (inPut == magNum){
                 document.getElementById("output").innerHTML = ("You Win!!!");
-                gmOv();
+                gmOv();                                   // call game over function
             }                                            // If statement for clicks
         clKs();                                            //  calling clKs function
         }else{                                                     //else to end display and disable button
@@ -43,15 +40,13 @@
     return false;
     };
 
-    function gmOv(){
-        console.log("in gmOv");
+    function gmOv(){                                     //game over function - disables button
         var inBt = document.querySelector("button");       // variable to assign button
         inBt.innerHTML = ("DONE!!!");                            // assign DONE!!!
         document.querySelector("button").disabled=true;           //DOM to disable button
     }
 
     function clKs(){                                             // click function
-        console.log("in clKs");
         document.querySelectorAll("button").value = ++cliKs;     // getElementById to add up clicks
     }
 
